e7f9c68ea900e060c464c1de9501c53224d59fdf
Leandro Jorquera <leandrovalero2009@gmail.com>
2022-11-21 21:38:50 +0100

    update crispresso input code

--- CRISPResso.py
+++ CRISPResso.py
@@ -1,13 +1,28 @@
 import os
 import pandas as pd
+import numpy as np
+import re
+#-- path where reads are stored
 path = "/Users/leandrojorqueravalero/Desktop/PhD/Miseq/Miseq_optimization"
-#list directory
+#-- loop for creating file names and abbreviations
+header = ['name','fastq_r1']
+list_fastq = []
+list_name = []
 for file in os.listdir(path):
-    #only fastq.gz files
+    #only fastq.gz files and generate abbreviated names (ID)
     if file.endswith(".fastq.gz"):
-        print(file)
-
-
+        list_fastq.append(file)
+        short = re.split("[_S]",file)[0]
+        list_name.append(short)
+#print(list_name)
+#print(list_ID)
+#--turning results into dataframe
+dict = {'name':list_name,'fastq_r1':list_fastq}
+df = pd.DataFrame(dict)
+df.columns = ['name','fastq_r1']
+print(df)
+#--saving the file
+df.to_csv("input_file", sep="\t", index=False)
 
 
 #data1 = pd.read_csv("/Users/leandrojorqueravalero/Desktop/PhD/Miseq/Miseq_optimization", sep='\t')
