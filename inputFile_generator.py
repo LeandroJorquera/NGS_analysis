1034546279a6cb8ace6a3fa25a11ac3ac2d1d71b
Leandro Jorquera <leandrovalero2009@gmail.com>
2022-11-24 12:44:42 +0100

    no print df

--- inputFile_generator.py
+++ inputFile_generator.py
@@ -20,23 +20,10 @@ for file in os.listdir(path):
 dict = {'name':list_name,'fastq_r1':list_fastq}
 df = pd.DataFrame(dict)
 df.columns = ['name','fastq_r1']
-print(df)
+#print(df)
 #--saving the file
 df.to_csv("input_file", sep="\t", index=False)
 
-
-#data1 = pd.read_csv("/Users/leandrojorqueravalero/Desktop/PhD/Miseq/Miseq_optimization", sep='\t')
-
-#data2 = pd.read_csv("/Users/leandrojorqueravalero/Desktop/PhD/Miseq/", sep='\t')
-#df2 = pd.DataFrame(data2)
-
-#df_final = df1.join(df2,lsuffix="_caller", rsuffix="_other")
-#df_final.columns = ['name','fastq_r1']
-
-#print(df_final)
-#data1 = pd.read_csv("/Users/leandrojorqueravalero/Desktop/PhD/Miseq/Miseq_optimization/editing_ouput.txt", sep='\t')
-#df1 = pd.DataFrame(data1)
-#print(df1)
 
 
 
