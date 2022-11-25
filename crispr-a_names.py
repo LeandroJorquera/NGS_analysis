3a9658c4f6f6b0775925dc824c4f500a702f975c
Leandro Jorquera Valero <ljorquera@mr-03-02.cls.upf.edu>
2022-11-25 16:48:15 +0100

    include crispr-A config file

---
+++ crispr-a_names.py
@@ -0,0 +1,37 @@
+import os
+import pandas as pd
+import numpy as np
+import re
+
+#-- path where reads are in my marvin folder
+path = "/homes/users/ljorquera/scratch/data/Miseq_optimization"
+
+#-- loop for creating file names and abbreviations
+header = ['name','fastq_r1']
+list_fastq = []
+list_name = []
+for file in os.listdir(path):
+    #only fastq.gz files and generate abbreviated names (ID)
+    if file.endswith(".fastq.gz"):
+        list_fastq.append(file)
+        short = re.split("[_S]",file)[0]
+        list_name.append(short)
+#print(list_name)
+#print(list_ID)
+
+#--turning results into dataframe
+dict = {'name':list_name,'fastq_r1':list_fastq}
+df = pd.DataFrame(dict)
+df.columns = ['name','fastq_r1']
+#print(df)
+
+#--saving the file
+df.to_csv("input_file", sep="\t", index=False)
+
+
+
+
+
+
+
+
