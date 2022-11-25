5182266222c706fab562789f6922d24da13d8b18
Leandro Jorquera Valero <ljorquera@mr-00-02.cls.upf.edu>
2022-11-25 11:09:38 +0100

    changes bash and py for cluster run

--- inputFile_generator.py
+++ inputFile_generator.py
@@ -23,13 +23,8 @@ for file in os.listdir(path):
 dict = {'name':list_name,'fastq_r1':list_fastq}
 df = pd.DataFrame(dict)
 df.columns = ['name','fastq_r1']
-
 #print(df)
 
-#--saving the file (directory where singularity is run)
-df.to_csv("input_file", sep="\t", index=False)
-
-#print(df)
 #--saving the file
 df.to_csv("input_file", sep="\t", index=False)
 
