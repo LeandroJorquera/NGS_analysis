51dc85ce81decb12a54481affc31815a3e13d91c
Leandro Jorquera <leandrovalero2009@gmail.com>
2022-11-30 11:12:48 +0100

    new inputfile generator containing R2

--- inputFile_generator.py
+++ inputFile_generator.py
@@ -7,26 +7,29 @@ import re
 path = "/homes/users/ljorquera/scratch/data/Miseq_optimization"
 
 #-- loop for creating file names and abbreviations
-header = ['name','fastq_r1']
 list_fastq = []
 list_fastq2 = []
 list_name = []
+
 for file in os.listdir(path):
-    #only fastq.gz files and generate abbreviated names (ID)
+    #only fastq.gz files and generate abbreviated names
     if file.endswith(".fastq.gz"):
-        if file.find("R1"):
+        if filenmatch:
             list_fastq.append(file)
-        if file.find("R2"):
-            list_fastq2.append(file)
+        #if file.find("R2"):
+            #list_fastq2.append(file)
         short = re.split("[_S]",file)[0]
         list_name.append(short)
-#print(list_name)
+print(list_name)
+print(list_fastq)
 
 #--turning results into dataframe
 dict = {'name':list_name,'fastq_r1':list_fastq, 'fastq_r2':list_fastq2}
 df = pd.DataFrame(dict)
 df.columns = ['name','fastq_r1','fastq_r2']
-print(df)
+# remove duplicates
+df = df.drop_duplicates(subset ='name')
+#print(df)
 
 #--saving the file
 #df.to_csv("input_file", sep="\t", index=False)
